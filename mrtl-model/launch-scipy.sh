794c73bf0acdc3fe266a7fbb08c6fdfb181708f3
rrichardtang <c3u2b4e@gmail.com>
2022-06-23 22:18:01 -0700

    Reduced #GPU to 1

--- mrtl-model/launch-scipy.sh
+++ mrtl-model/launch-scipy.sh
@@ -5,7 +5,7 @@ set -a  # mark all variables below as exported (environment) variables
 # Indentify this script as source of job configuration
 K8S_CONFIG_SOURCE=${BASH_SOURCE[0]}
 
-K8S_NUM_GPU=2  # max of 2 (contact ETS to raise limit)
+K8S_NUM_GPU=1  # max of 2 (contact ETS to raise limit)
 K8S_NUM_CPU=8  # max of 8 ("")
 K8S_GB_MEM=64  # max of 64 ("")
 K8S_PRIORITY_CLASS_NAME=normal
